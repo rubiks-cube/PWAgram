f6357802cd41d0dcbd22f50b7e12f43a57b3891e
Abhishek Ranjan <abhi03iiitm@gmail.com>
2018-01-22 21:46:40 +0530

    'workbox1'

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  "globDirectory": "public\\",
+  "globDirectory": "public/",
   "globPatterns": [
-    "**/*.{html,ico,json,css,png,jpg,js}"
+    "**/*.{html,ico,json,css}",
+    "src/images/*.{jpg,png}",
+    "src/js/*.min.js"
   ],
+  "swSrc": "public/sw-base.js",
   "swDest": "public/service-worker.js",
   "globIgnores": [
-    "..\\workbox-cli-config.js"
+    "../workbox-cli-config.js",
+    "help/**",
+    "404.html"
   ]
 };
